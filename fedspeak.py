2e25cb19b19f3e242c49f3d4b3e6d36444a1275f
Darren Chang <darren.ch12@gmail.com>
2020-05-29 18:23:57 -0400

    a mess, working on stacking

--- fedspeak.py
+++ fedspeak.py
@@ -60,29 +60,29 @@ def log_error(e):
     """
     print(e)
 
-links_use = links['url'] #extract urls from links
-links_use = links_use.values.tolist() #coerce to list
+# links_use = links['url'] #extract urls from links
+# links_use = links_use.values.tolist() #coerce to list
 
-fed_text_raw = pd.DataFrame(columns = ['url', 'text']) #empty df
+# fed_text_raw = pd.DataFrame(columns = ['url', 'text']) #empty df
 
-# process usually takes 1.5 - 2.5 hrs. iterates across all the urls, grabs the text as a list and adds to larger dataframe
-for url in links_use:
-    text = simple_get(url)
-    df = pd.DataFrame({'url': url, 'text': [text]})
-    fed_text_raw = fed_text_raw.append(df, ignore_index= True)
+# # process usually takes 1.5 - 2.5 hrs. iterates across all the urls, grabs the text as a list and adds to larger dataframe
+# for url in links_use:
+#     text = simple_get(url)
+#     df = pd.DataFrame({'url': url, 'text': [text]})
+#     fed_text_raw = fed_text_raw.append(df, ignore_index= True)
 
-fed_text_raw = pd.DataFrame(fed_text_raw)
+# fed_text_raw = pd.DataFrame(fed_text_raw)
 
-# merging with original dataframe
-fed_text_all = pd.merge(links, fed_text_raw, how = 'outer', on = 'url')
+# # merging with original dataframe
+# fed_text_all = pd.merge(links, fed_text_raw, how = 'outer', on = 'url')
 
-# store raw data as hdf -- too large for pickle oops
-fed_text_all.to_hdf('fedtext.h5', key='fed_text_all', mode='w', format='table')
+# # store raw data as hdf -- too large for pickle oops
+# fed_text_all.to_hdf('fedtext.h5', key='fed_text_all', mode='w', format='table')
 
 # back to testing
 
 #### TESTING
-urls = pd.DataFrame(links.loc[0:50,:])
+urls = pd.DataFrame(links.loc[0:20,:])
 urls = urls['url']
 urls = urls.values.tolist()
 
@@ -93,17 +93,37 @@ fed_text_raw_test = pd.DataFrame()
 for url in urls:
     text = simple_get(url)
     df = pd.DataFrame({'url': url, 'text': [text]})
-    fed_text_raw_test = fed_text_raw_test.append(df, ignore_index= True)
+    fed_text_raw_test = fed_text_raw_test.append(df, ignore_index = True)
 # fed_text_raw = pd.concat(fed_text_raw)
 
 fed_text_raw_test.columns = fed_text_raw_test.columns.str.strip()
 
-(fed_text_raw_test
-    .groupby(['url'], group_keys=False)
-    .apply(lambda g: stack().reset_index(drop=True)))
-    #.assign(text = text.apply(pd.Series).stack().reset_index(drop=True)))
+fed_text_test = fed_text_raw_test.copy()
 
-fed_text_test_stack['text'] = fed_text_raw_test['text'].apply(pd.Series).stack().reset_index(drop=True)
+
+fed_text_test.index = urls
+fed_text_test['text'] = fed_text_test['text'].apply(pd.Series).stack().reset_index(drop = True)
+
+fed_text_groups = (fed_text_test
+    .groupby(['url'], group_keys=False))
+
+fed_text_raw_test.stack(level = 0)
+
+fed_text_groups = group_by_url.stack()
+
+fed_text_groups['text'] = pd.concat(fed_text_groups['text']).reset_index(drop = True)
+
+.apply(lambda g: g['text'].stack().reset_index(drop = True)))
+    #.assign(text = text.apply(pd.Series).stack().reset_index(drop = True)))
+
+fed_text_test_stack
+
+
+fed_text_groups = fed_text_groups.unstack()
+
+fed_text_test['text'].stack()
+
+.apply(pd.Series).unstack().reset_index(drop = True)
 
 end = time.time()
 print(end - start)
