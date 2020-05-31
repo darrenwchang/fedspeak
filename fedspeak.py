9c82df3450920fe2546d32b48121762b342ea172
Darren Chang <darren.ch12@gmail.com>
2020-05-30 22:40:20 -0400

    wrote things into functions

--- fedspeak.py
+++ fedspeak.py
@@ -1,7 +1,7 @@
-#######fedspeak
-###darren chang
+####### fedspeak
+### darren chang
 
-####SETUP
+#### SETUP
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -31,8 +31,8 @@ os.chdir('C:\\Users\\darre\\Documents\\_econ\\fedspeak')
 links = pd.read_csv('links.csv')
 links.head(10)
 
-####SCRAPING
-# functions for scraping, with error logging
+#### SCRAPING
+# functions for getting URLs, with error logging
 def simple_get(url):
     """
     Attempts to get the content at `url` by making an HTTP GET request.
@@ -67,13 +67,15 @@ def log_error(e):
     """
     print(e)
 
-def scrape(links, #dataframe of urls and other informaiton
-h5name #name of h5 file):
+# scraping a set of links
+def scrape(links, #dataframe of urls and other info
+):
     """
     function for scraping set of links to dataframe
     """
     links_use = links['url'].values.tolist() #extract urls as list
-    fed_text_raw = pd.DataFrame(columns = ['url', 'text']) #empty df
+    fed_text_raw = pd.DataFrame() #empty df
+    # fed_text_raw = pd.DataFrame(columns = ['url', 'text']) #empty df with columns
 
     # process usually takes 1.5 - 2.5 hrs. 
     # iterates across all the urls
@@ -86,107 +88,88 @@ h5name #name of h5 file):
     fed_text_raw.columns = fed_text_raw.columns.str.strip() #strip column names 
     
     # merging with original dataframe
-    fed_text_all = pd.merge(links, fed_text_raw, how = 'outer', on = 'url')
+    # fed_text_all = pd.merge(links, fed_text_raw, how = 'outer', on = 'url')
 
-    return fed_text_all
+    return fed_text_raw
 
-# store raw data as hdf -- too large for pickle oops
-fed_text_all.to_hdf('fedtext.h5', key='fed_text_all', mode='w', format='table')
+start = time.time()
 
+# fed_text_raw = scrape(links)
+linklist = pd.DataFrame(links.loc[0:20,:])
+fed_text_raw = scrape(linklist) #test run
 
-#### TESTING
+end = time.time()
+print(end - start)
 
-def preprocess(text):
-    clean_data = []
-#    for x in (text[:][0]): #this is Df_pd for Df_np (text[:])
-    for x in text:
-        new_text = re.sub('<.*?>', '', x)   # remove HTML tags
-        new_text = re.sub(r'[^\w\s]', '', new_text) # remove punc.
-        new_text = re.sub(r'\d+','',new_text)# remove numbers
-        new_text = new_text.lower() # lower case, .upper() for upper          
-        if new_text != '':
-            clean_data.append(new_text)
-    return clean_data
-
+#### CLEANING
+# this function for preprocessing is finnicky - may be something with the string type
 def preprocess(df, 
  text_field, # field that has text
  new_text_field_name # name of field that has normalized text
  ):
     """
-    normalizes text by converting it to lowercase and removing characters that do not contribute to natural text meaning
+    normalizes dataframes by converting it to lowercase and removing characters that do not contribute to natural text meaning
     """
     df[new_text_field_name] = df[text_field].str.lower()
     df[new_text_field_name] = df[new_text_field_name].apply(
-        lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem)
-        ) # strips out numbers
-    # df[new_text_field_name] = df[new_text_field_name].apply(
-    #     lambda elem: re.sub(r'<.*?>',"",elem) 
-    #     ) # strips out tags
+        lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", 
+        "", elem)
+        )  
+    df[new_text_field_name] = df[new_text_field_name].apply(
+        lambda elem: re.sub("<.*?>","", elem) 
+        ) # strips out tags
     df[new_text_field_name] = df[new_text_field_name].apply(
         lambda elem: re.sub(r"\d+", "", elem)
         )
     
     return df
 
-def  preprocess(df, text_field, new_text_field_name):
-    df[new_text_field_name] = df[text_field].str.lower()
-    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))  
-    # remove numbers
-    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"\d+", "", elem))
-    
-    return df
+def unnest(df, # line-based dataframe
+                  column_to_tokenize, # name of the column with the text
+                  new_token_column_name, # what you want the column of words to be called
+                  tokenizer_function, #what tokenizer to use
+                  original_list): # original list of data
 
-linklist = pd.DataFrame(links.loc[0:20,:])
-urls = linklist['url'].values.tolist()
+    return (df[column_to_tokenize]
+                .apply(str)
+                .apply(tokenizer_function)
+                .apply(pd.Series)
+                .stack()
+                .reset_index(level=0)
+                .set_index('level_0')
+                .rename(columns={0: new_token_column_name})
+                .join(df.drop(column_to_tokenize, 1), how='left')
+                .reset_index(drop=True)
+                .merge(original_list, how = 'outer', on = 'url')
+              )
 
-start = time.time()
-
-fed_text_raw_test = pd.DataFrame()
-
-# this works
-for url in urls:
-    text = simple_get(url)
-    df = pd.DataFrame({'url': url, 'text': [text]})
-    fed_text_raw_test = fed_text_raw_test.append(df, ignore_index = True)
-# fed_text_raw = pd.concat(fed_text_raw)
-
-fed_text_raw_test.columns = fed_text_raw_test.columns.str.strip()
-
-fed_text_test = fed_text_raw_test.copy()
-
-fed_text_test['text'] = fed_text_test['text'].apply(str)
-
-preprocess(fed_text_test, 'text', 'text')
-
-(fed_text_test['text']
-    .apply(str)
-    .apply(nltk.word_tokenize)
-    .apply(pd.Series)
-    .stack()
-    .apply(preprocess)
-    .reset_index(level=0)
-    .set_index('level_0')
-    .rename(columns={0: 'word'})
-    .join(fed_text_test.drop('text', 1), how = 'left')
-    .reset_index(drop = True)
-    .merge(linklist, how = 'outer', on = 'url')
-    )
-
-# # reading hdf (for later use, so you don't have to keep scraping the MN Fed's website)
-# fed_text_hdf = pd.read_hdf('fedtext.h5', 'fed_text_all')
-
-#### CLEANING
-start = time.time()
-fed_text_token = fed_text_all.copy
-fed_text_token['text'] = fed_text_token['text'].apply(lambda row: nltk.sent_tokenize(row['text']), axis=1)
-
-fed_text_token['text'] = fed_text_all['text'].apply(nltk.word_tokenize)
-fed_text_token.to_csv('fed_text_token.csv', index = False)
+fed_text_raw = preprocess(fed_text_raw, 'text', 'text')
+fed_text_all = unnest(fed_text_raw, 'text', 'word', nltk.word_tokenize, linklist)
+fed_text_all.to_hdf('fedtext.h5', key='fed_text_all', mode='w', format='table') # save as hdf
+fed_text_all.to_csv('fed_text_all.csv', index = False) # save as csv
 
 end = time.time()
 print(end - start)
 
-# not working
-#  stop_sent = ['Back to Archive', 'serve the public', 'We examine economic issues',  'conduct world-class','We provide the banking community','We strive to advance policy']
-#  fed_text_all[~fed_text_all['text'].str.contains('|'.join(stop_sent))]
-# fed_text_raw
+### working test code
+
+# for url in urls:
+#     text = simple_get(url)
+#     df = pd.DataFrame({'url': url, 'text': [text]})
+#     fed_text_raw_test = fed_text_raw_test.append(df, ignore_index = True)
+
+# (fed_text_test['text']
+#     .apply(str)
+#     .apply(nltk.word_tokenize)
+#     .apply(pd.Series)
+#     .stack()
+#     .reset_index(level=0)
+#     .set_index('level_0')
+#     .rename(columns={0: 'word'})
+#     .join(fed_text_test.drop('text', 1), how = 'left')
+#     .reset_index(drop = True)
+#     .merge(linklist, how = 'outer', on = 'url')
+#     )
+
+# # reading hdf (for later use, so you don't have to keep scraping the MN Fed's website)
+# fed_text_hdf = pd.read_hdf('fedtext.h5', 'fed_text_all')
