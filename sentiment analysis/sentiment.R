7766518a60ff1d048f6aa28a3d7affc8f20ae9a0
Darren Chang <darren.ch12@gmail.com>
2020-06-02 17:53:29 -0400

    base visualization

--- sentiment analysis/sentiment.R
+++ sentiment analysis/sentiment.R
@@ -12,19 +12,42 @@ fed_text_all <- vroom('C:\\Users\\darre\\Documents\\_econ\\fedspeak\\text mining
 fed_sentiment <- 
     fed_text_all %>% 
     inner_join(get_sentiments("loughran")) %>% 
-    count(report, sentiment, date) %>% 
+    count(report, year, date, sentiment) %>% 
     spread(sentiment, n, fill = 0) %>% 
     mutate(sentiment = (positive - negative)/(positive+negative)) %>% 
-    mutate(date = as.Date(date, format = "%m/%d/%Y"))
+    mutate(date = as.Date(date, format = "%m/%d/%Y")) %>% 
+    filter(sentiment != 1) # this data is not available on the Minneapolis Fed website
 
-ggplot(fed_sentiment,
-    aes(date, sentiment, fill = sentiment>0)) +
-    geom_col(show.legend = FALSE) +
-    scale_fill_manual(values=c("red","#27408b")) +
-    #facet_wrap(~report, ncol = 8, scales = "free_x")+
+# bar plot
+# ggplot(fed_sentiment,
+#     aes(date, sentiment, fill = sentiment > 0)) +
+#     geom_col(show.legend = FALSE) +
+#     scale_fill_manual(values=c("red","#27408b")) +
+#     #facet_wrap(~report, ncol = 8, scales = "free_x")+
+#     theme_fivethirtyeight() +
+#     labs(x="report (~8 per year)",
+#         y="sentiment",
+#         title="Sentiment in Federal Reserve Beige Book",
+#         subtitle="customized Loughran lexicon\npolarity = (positive-negative)/(positive+negative)",
+#         caption="@darrenwchang\nSource: Beige Book")
+
+# line plot
+g1 <- ggplot(fed_sentiment,
+    aes(x = date, y = sentiment)) +
+    geom_line(color = 'blue') +
     theme_fivethirtyeight() +
-    labs(x="report (~8 per year)",
-        y="sentiment",
-        title="Sentiment in Federal Reserve Beige Book",
-        subtitle="customized bing lexicon\nsentiment = (positive-negative)/(positive+negative)",
-        caption="@thedarrenchang\nSource: Beige Book")
+    scale_x_date(breaks = "5 years", 
+    limits = as.Date(c("1970-01-01","2020-01-01")),
+    date_labels = "%Y") +
+    labs(x = "Beige Book Report (~8/year)",
+        y = "polarity",
+        title = "Sentiment in Federal Reserve Beige Book",
+        subtitle = "customized Loughran lexicon\npolarity = (positive-negative)/(positive+negative)",
+        caption = "@darrenwchang\nSource: Federal Reserve Bank of Minneapolis")
+
+g1
+
+ggsave("sentiment_base.png", plot = g1, device = png())
+
+# use fredr for this
+# https://fred.stlouisfed.org/categories/32071
