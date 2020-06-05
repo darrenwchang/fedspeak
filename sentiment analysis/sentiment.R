f7130484aa1996c7d36b636f4c80f38d07fec076
Darren Chang <darren.ch12@gmail.com>
2020-06-05 01:31:44 -0400

    edited documentation

--- sentiment analysis/sentiment.R
+++ sentiment analysis/sentiment.R
@@ -6,8 +6,10 @@ library(tidytext)
 library(vroom)
 library(ggthemes)
 library(zoo)
+library(viridis)
 
 setwd("C:\\Users\\darre\\Documents\\_econ\\fedspeak\\sentiment analysis")
+# setwd("C:\\Users\\darre\\Documents\\_econ\\fedspeak")
 fed_text_all <- vroom('C:\\Users\\darre\\Documents\\_econ\\fedspeak\\text mining\\fed_text_all.csv') # read csv
 
 fed_sentiment <- 
@@ -20,7 +22,7 @@ fed_sentiment <-
     mutate(sentiment = (positive - negative)/(positive + negative)) %>%
     mutate(date = as.Date(date, format = "%m/%d/%Y")) %>% 
     filter(sentiment != 1) %>%  # this data is not available on the Minneapolis Fed website
-    filter(date != "2015-07-01")
+    filter(date != "2015-07-01") %>% 
     mutate(sent_ma = rollmean(sentiment, k = 3, fill = NA))
 
 fed_sentiment_bank <- 
@@ -36,11 +38,35 @@ fed_sentiment_bank <-
     mutate(date = as.Date(date, format = "%m/%d/%Y")) %>% 
     filter(sentiment != 1) %>%
     filter(date != "2015-07-01") %>% 
-    count(date, sent_norm, bank) %>% 
+    select(date, bank, sent_norm, sentiment) %>% 
+    pivot_longer(-c(date, bank), names_to = "transformation", values_to = "value") %>% 
+    mutate(transformation = as_factor(transformation))
+
+fed_sentiment_scale <- 
+    fed_text_all %>% 
+    inner_join(get_sentiments("loughran")) %>% # or bing
+    # inner_join(get_sentiments("bing")) %>%
+    count(report, year, date, bank, sentiment) %>% 
+    spread(sentiment, n, fill = 0) %>% 
+    mutate(sentiment = (positive - negative)/(positive+negative)) %>%
+    group_by(bank) %>% 
+    mutate(sent_norm = scale(sentiment)) %>% 
+    ungroup() %>%
+    mutate(date = as.Date(date, format = "%m/%d/%Y")) %>% 
+    filter(sentiment != 1) %>%
+    filter(date != "2015-07-01") %>% 
+    select(date, sent_norm, bank, sentiment) %>% 
     group_by(date) %>% 
-    summarize(mean = mean(sent_norm), sum = sum(sent_norm)) %>% 
-    mutate(sent_ma = rollmean(mean, k = 3, fill = NA)) %>%
-    mutate(sent_ma_sum = rollmean(sum, k = 3, fill = NA)) 
+    summarize(norm_mean = mean(sent_norm),
+                # norm_sum = sum(sent_norm), 
+                # sent_sum = sum(sentiment),
+                sent_mean = mean(sentiment)) %>% 
+    mutate(sent_norm_mean_ma = rollmean(norm_mean, k = 3, fill = NA)) %>%
+    # mutate(sent_norm_sum_ma = rollmean(norm_sum, k = 3, fill = NA)) %>%
+    mutate(sent_mean_ma = rollmean(sent_mean, k = 3, fill = NA)) %>%
+    # mutate(sent_sum_ma = rollmean(sent_sum, k = 3, fill = NA))  %>% 
+    pivot_longer(-date, names_to = "transformation", values_to = "value") %>% 
+    mutate(transformation = as_factor(transformation))
 
 # bar plot
 # ggplot(fed_sentiment,
@@ -69,15 +95,15 @@ g1 <- ggplot(fed_sentiment,
         y = "polarity",
         title = "Sentiment in Federal Reserve Beige Book",
         subtitle = "customized Loughran lexicon\npolarity = (positive-negative)/(positive+negative)",
-        caption = "@darrenwchang\nSource: Federal Reserve Bank of Minneapolis") +
+        caption = "@darrenwchang\nSource: Federal Reserve Bank of Minneapolis")
 
 g1
 
 ggsave("sentiment_base.png", plot = g1, device = png())
 
 g2 <- ggplot(fed_sentiment_bank,
-    aes(x = date, y = sent_ma)) +
-    geom_line(color = 'blue') +
+    aes(x = date, y = value, color = transformation)) +
+    geom_line() +
     theme_fivethirtyeight() +
     # scale_x_date(limits = as.Date(c("1970-01-01","2020-01-01")),
     # date_labels = "%Y") +
@@ -92,9 +118,10 @@ g2
 
 ggsave("sentiment_base_bank.png", plot = g2, device = png())
 
-ggplot(fed_sentiment_bank,
-    aes(x = date, y = sent_ma)) +
-    geom_line(color = 'blue') +
+ggplot(filter(fed_sentiment_scale, transformation == "sent_norm_mean_ma" | transformation == "norm_mean"),
+    aes(x = date, y = value, color = transformation)) +
+    geom_line() +
+    #scale_color_viridis() +
     theme_fivethirtyeight() +
     # scale_x_date(limits = as.Date(c("1970-01-01","2020-01-01")),
     # date_labels = "%Y") +
--- text mining/text_mining.py
+++ text mining/text_mining.py
@@ -4,7 +4,6 @@
 ## ---- setup
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 import os #setwd
 import time #timing
